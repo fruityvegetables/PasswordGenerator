f434744fc897eb68214e617b337bf16698560ab7
fruityvegetables <gg.troyer@gmail.com>
2020-03-19 18:52:08 -0500

    second changes

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', '
 // array for uppercase letters
 var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 //
-var passwordArray = ['numericCharacters', 'specialCharacters', 'lowerCaseLetters', 'upperCaseLetters'];
+var passwordArray = [];
 
+var blankPassword = '';
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.textContent = password;
 
 }
 
@@ -25,31 +26,83 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-function generatePassword() { }
-var passwordLength = prompt("How long would you like your password? Please enter an integer between 8-128.");
-console.log(passwordLength);
-var lowerCase = confirm("Press OK for lowercase or CANCEL for none.");
-console.log(lowerCase);
-var upperCase = confirm("Press OK for uppercase or CANCEL for none.");
-console.log(upperCase);
-var numeric = confirm("Press OK for numeric or CANCEL for none.");
-console.log(numeric);
-var specialCharOption = confirm("Press OK for special characters or CANCEL for none.");
-console.log(specialCharOption);
+function generatePassword() {
 
-if (lowerCase == true) {
-  generatePassword(lowerCaseLetters);
+  passwordArray = [];
+  blankPassword = '';
+  
+  var passwordLength = prompt("How long would you like your password? Please enter an integer between 8-128.");
+  // if (passwordLength >= 8 && passwordLength <= 128) {
+  //   alert('This number is within the parameters: ' + passwordLength);
+  //   console.log(passwordLength);
 
-} if (upperCase == true) {
-  generatePassword(upperCaseLetters);
+  var upperCase = confirm("Press OK for uppercase or CANCEL for none.");
+  console.log(upperCase);
 
-} if (numeric == true) {
-  generatePassword(numericCharacters);
+  var numeric = confirm("Press OK for numeric or CANCEL for none.");
+  console.log(numeric);
 
-} if (specialCharOption == true) {
-  generatePassword(specialCharacters);
+  var specialCharOption = confirm("Press OK for special characters or CANCEL for none.");
+  console.log(specialCharOption);
 
+  var lowerCase = confirm("Press OK for lowercase or CANCEL for none.");
+  console.log(lowerCase);
+
+  if (lowerCase) {
+    passwordArray = blankPassword.concat(lowerCaseLetters);
+
+  }
+
+  if (upperCase) {
+    passwordArray = blankPassword.concat(upperCaseLetters);
+
+  }
+
+  if (numeric) {
+    passwordArray = blankPassword.concat(numericCharacters);
+
+  }
+
+  if (specialCharOption) {
+    passwordArray = blankPassword.concat(specialCharacters);
+
+  }
+
+
+  for (i = 0; i < passwordLength; i++) {
+    passwordLength += passwordArray[Math.floor(Math.random() * passwordArray.length)];
+
+  }
+
+  return blankPassword;
 }
 
+
+
+
+
+
+
+    // if (upperCase == true) {
+    //   generatePassword(upperCaseLetters);
+
+    // }
+
+
+    // if (numeric == true) {
+    //   generatePassword(numericCharacters);
+
+    // }
+
+
+    // if (specialCharOption == true) {
+    //   generatePassword(specialCharacters);
+
+    // }
+
+  // } else {
+  //   alert('NOPE, this is not within parameters: ' + passwordLength);
+  //   console.log(passwordLength);
+  // }
 
 
